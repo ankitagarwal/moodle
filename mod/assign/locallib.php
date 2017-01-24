9dd56616a7bd16f89abc99ca22e8c9d13d657947
Ankit Agarwal <ankit@moodle.com>
2017-01-24 17:42:40 +0530

    MDL-57362 assignlib: Fix minor bug in phpdocs

--- mod/assign/locallib.php
+++ mod/assign/locallib.php
@@ -3035,7 +3035,7 @@ class assign {
      * Throw an error if the permissions to view this users submission are missing.
      *
      * @throws required_capability_exception
-     * @return none
+     * @return void
      */
     public function require_view_submission($userid) {
         if (!$this->can_view_submission($userid)) {
@@ -3047,7 +3047,7 @@ class assign {
      * Throw an error if the permissions to view grades in this assignment are missing.
      *
      * @throws required_capability_exception
-     * @return none
+     * @return void
      */
     public function require_view_grades() {
         if (!$this->can_view_grades()) {
